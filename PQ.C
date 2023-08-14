f5f358c5363e49dce308ce02f1b2594efd6f641f
Sandip Paul <119481806+03Sandip@users.noreply.github.com>
2023-08-14 20:42:26 +0530

    Add files via upload

---
+++ PQ.C
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<conio.h>
+#include<process.h>
+#define QUEUE_SIZE 5
+
+int qfull(int r)
+{
+ return(r==QUEUE_SIZE-1)?1:0;
+}
+int insert_rear(int item, int q[], int *r)
+{
+ int j;
+ if(qfull(*r))
+ {
+  printf("queue overflow\n");
+  getch();
+  return;
+ }
+ j=*r;  //compare from the initial position
+ //find the appropiate position
+while(j>=0 && item<q[j])
+{
+ q[j+1]=q[j];  // move the item at q[j] to its next position
+ j--;
+}
+// insert the item at appropiate position
+q[j+1]=item;
+*r=*r+1;
+return;
+}
+
+int qempty(int f,int r)
+{
+ return(f>r)?1:0;
+}
+
+void delete_front(int q[], int *f,int *r)
+{
+ if(qempty(*f,*r))
+ {
+  printf("queue underflow\n");
+  *f=0; *r=-1;
+  getch();
+  return;
+  }
+ printf("the element deleted is %d\n",q[(*f)++]);
+ getch();
+}
+
+void display(int q[], int f, int r)
+{
+ int i;
+ if(qempty(f,r))
+ {
+  printf("queue is empty\n");
+  getch();
+  return;
+ }
+ printf("contents of the queue is \n");
+ for(i=f;i<=r;i++)
+  printf("%d\t",q[i]);
+ getch();
+}
+
+void main()
+{
+ int choice, item,f,r,q[10];
+ clrscr();
+ f=0;
+ r=-1;
+ for(;;)
+ {
+  clrscr();
+  printf("\n piority queue operations");
+  printf("\n 1. Insert");
+  printf("\n 2. Delete");
+  printf("\n 3. Display");
+  printf("\n 4. Exit");
+  printf("\n Enter your choice");
+  scanf("%d",&choice);
+  switch(choice)
+  {
+   case 1:
+	  printf("\nenter the item to be inserted:");
+	  scanf("%d",&item);
+	  insert_rear(item,q,&r);
+	  break;
+   case 2:
+	  delete_front(q,&f,&r);
+	  break;
+   case 3:
+	  display(q,f,r);
+	  break;
+   default:
+	  exit(0);
+  }
+ }
+}
