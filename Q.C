83e59d67176132b427e20504b823a4e070d912bb
Sandip Paul <119481806+03Sandip@users.noreply.github.com>
2023-09-07 19:47:35 +0530

    Update Q.C

--- Q.C
+++ Q.C
@@ -1,88 +1,85 @@
-#include<stdio.h>
-#include<conio.h>
-#include<process.h>
+#include <stdio.h>
+#include <stdlib.h> 
+
 #define QUEUE_SIZE 5
 
 int qfull(int r)
 {
- return(r==QUEUE_SIZE-1)?1:0;
-}
-int insert_rear(int item, int q[], int *r)
-{
- if(qfull(*r))
- {
-  printf("queue overflow\n");
-  getch();
-  return;
- }
- q[++(*r)]=item;
- return;
+    return (r == QUEUE_SIZE - 1) ? 1 : 0;
 }
 
-int qempty(int f,int r)
+void insert_rear(int item, int q[], int *r)
 {
- return(f>r)?1:0;
+    if (qfull(*r))
+    {
+        printf("Queue overflow\n");
+        return;
+    }
+    q[++(*r)] = item;
 }
 
-void delete_front(int q[], int *f,int *r)
+int qempty(int f, int r)
 {
- if(qempty(*f,*r))
- {
-  printf("queue underflow\n");
-  *f=0; *r=-1;
-  getch();
-  return;
-  }
- printf("the element deleted is %d\n",q[(*f)++]);
- getch();
+    return (f > r) ? 1 : 0;
+}
+
+void delete_front(int q[], int *f, int *r)
+{
+    if (qempty(*f, *r))
+    {
+        printf("Queue underflow\n");
+        *f = 0;
+        *r = -1;
+        return;
+    }
+    printf("The element deleted is %d\n", q[(*f)++]);
 }
 
 void display(int q[], int f, int r)
 {
- int i;
- if(qempty(f,r))
- {
-  printf("queue is empty\n");
-  getch();
-  return;
- }
- printf("contents of the queue is \n");
- for(i=f;i<=r;i++)
-  printf("%d\t",q[i]);
- getch();
+    int i;
+    if (qempty(f, r))
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Contents of the queue:\n");
+    for (i = f; i <= r; i++)
+        printf("%d\t", q[i]);
+    printf("\n");
 }
 
-void main()
+int main()
 {
- int choice, item,f,r,q[10];
- clrscr();
- f=0;
- r=-1;
- for(;;)
- {
-  clrscr();
-  printf("\n general queue operations");
-  printf("\n 1. Insert");
-  printf("\n 2. Delete");
-  printf("\n 3. Display");
-  printf("\n 4. Exit");
-  printf("\n Enter your choice");
-  scanf("%d",&choice);
-  switch(choice)
-  {
-   case 1:
-	  printf("\nenter the item to be inserted:");
-	  scanf("%d",&item);
-	  insert_rear(item,q,&r);
-	  break;
-   case 2:
-	  delete_front(q,&f,&r);
-	  break;
-   case 3:
-	  display(q,f,r);
-	  break;
-   default:
-	  exit(0);
-  }
- }
+    int choice, item, f = 0, r = -1, q[QUEUE_SIZE];
+
+    while (1)
+    {
+        printf("\nGeneral Queue Operations");
+        printf("\n1. Insert");
+        printf("\n2. Delete");
+        printf("\n3. Display");
+        printf("\n4. Exit");
+        printf("\n\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter the item to be inserted: ");
+            scanf("%d", &item);
+            insert_rear(item, q, &r);
+            break;
+        case 2:
+            delete_front(q, &f, &r);
+            break;
+        case 3:
+            display(q, f, r);
+            break;
+        default:
+            exit(0);
+        }
+    }
+
+    return 0;
 }
