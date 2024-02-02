93fc849e5def183150b9c35521023d570d92e937
Sandip Paul <119481806+03Sandip@users.noreply.github.com>
2024-02-02 11:17:53 +0530

    Update bubbleshort.C

--- bubbleshort.C
+++ bubbleshort.C
@@ -1,43 +1,36 @@
-#include<stdio.h>
- 
-void printArray(int* A, int n){
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
- 
-void bubbleSortAdaptive(int *A, int n){
-    int temp;
-    int isSorted = 0;
-    for (int i = 0; i < n-1; i++)          // For number of pass
-    {
-        printf("Working on pass number %d\n", i+1 );
-        isSorted = 1;
-        for (int j = 0; j <n-1-i ; j++) // For comparison in each pass
-        {
-            if(A[j]>A[j+1]){
-                temp = A[j];
-                A[j] = A[j+1];
-                A[j+1] = temp;
-                isSorted = 0;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
             }
-        } 
-        if(isSorted){
-            return;
         }
-    } 
+    }
 }
- 
-int main()
- {
-     // int A[] = {12, 54, 65, 7, 23, 9};
-    int A[] = {1, 2, 5, 6, 12, 54, 625, 7, 23, 9, 987};
-    // int A[] = {1, 2, 3, 4, 5, 6};
-    int n = 11;
-    printArray(A, n);          // Printing the array before sorting
-    bubbleSortAdaptive(A, n); // Function to sort the array
-    printArray(A, n);        // Printing the array before sorting
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("\nSorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
- }
+}
