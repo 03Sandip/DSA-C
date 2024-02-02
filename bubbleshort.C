0b23d6c742ed38efee6904866e8e9a77e42c67f2
Sandip Paul <119481806+03Sandip@users.noreply.github.com>
2024-02-02 13:16:16 +0530

    Update bubbleshort.C

--- bubbleshort.C
+++ bubbleshort.C
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
 void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                swap(&arr[j], &arr[j+1]);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) 
+            {
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
