9d886cd8bab416f29605e7eda0095aa0a8a837eb
Sandip Paul <119481806+03Sandip@users.noreply.github.com>
2024-03-17 19:17:42 +0530

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a tree node
 struct TreeNode {
     int data;
     struct TreeNode* left;
     struct TreeNode* right;
 };
 
-// Function to create a new tree node
 struct TreeNode* createTreeNode(int data) {
     struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
     newNode->data = data;
@@ -16,40 +14,34 @@ struct TreeNode* createTreeNode(int data) {
     newNode->right = NULL;
     return newNode;
 }
-
-// Function to perform DFS traversal of a tree (preorder)
 void dfs_preorder(struct TreeNode* root) {
     if (root == NULL)
         return;
 
-    printf("%d ", root->data); // Visit the node
-    dfs_preorder(root->left);  // Traverse left subtree
-    dfs_preorder(root->right); // Traverse right subtree
+    printf("%d ", root->data); 
+    dfs_preorder(root->left);  
+    dfs_preorder(root->right); 
 }
 
-// Function to perform DFS traversal of a tree (inorder)
 void dfs_inorder(struct TreeNode* root) {
     if (root == NULL)
         return;
 
-    dfs_inorder(root->left);   // Traverse left subtree
-    printf("%d ", root->data); // Visit the node
-    dfs_inorder(root->right);  // Traverse right subtree
+    dfs_inorder(root->left);   
+    printf("%d ", root->data); 
+    dfs_inorder(root->right);  
 }
-
-// Function to perform DFS traversal of a tree (postorder)
 void dfs_postorder(struct TreeNode* root) {
     if (root == NULL)
         return;
 
-    dfs_postorder(root->left);  // Traverse left subtree
-    dfs_postorder(root->right); // Traverse right subtree
-    printf("%d ", root->data);  // Visit the node
+    dfs_postorder(root->left); 
+    dfs_postorder(root->right); 
+    printf("%d ", root->data);
 }
 
-// Sample usage
 int main() {
-    // Constructing a sample tree
+    
     struct TreeNode* root = createTreeNode(1);
     root->left = createTreeNode(2);
     root->right = createTreeNode(3);
@@ -57,18 +49,15 @@ int main() {
     root->left->right = createTreeNode(5);
     root->right->left = createTreeNode(6);
     root->right->right = createTreeNode(7);
-
-    // Perform DFS traversal (preorder)
+    
     printf("DFS preorder traversal of the tree: ");
     dfs_preorder(root);
     printf("\n");
 
-    // Perform DFS traversal (inorder)
     printf("DFS inorder traversal of the tree: ");
     dfs_inorder(root);
     printf("\n");
 
-    // Perform DFS traversal (postorder)
     printf("DFS postorder traversal of the tree: ");
     dfs_postorder(root);
     printf("\n");
